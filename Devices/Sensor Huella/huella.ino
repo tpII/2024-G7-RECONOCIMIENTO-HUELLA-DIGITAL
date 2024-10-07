fed175f8afd207e566f4ed766f2e540a0fc1c85d
Azul Martini <azul.martini@alu.ing.unlp.edu.ar>
2024-10-07 01:07:32 -0300

    Actualización de reconocimiento
    Busqueda en base de datos (falta implementar)

--- Devices/Sensor Huella/huella.ino
+++ Devices/Sensor Huella/huella.ino
@@ -9,18 +9,17 @@ int fingerPresentCount = 0;  // Contador para verificar si realmente hay un dedo
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial); // Espera a que se inicie la conexión serie
+  while (!Serial);  
   delay(100);
   Serial.println("Inicializando sensor de huellas...");
 
-  // Inicializa el sensor a 57600 baudios
   finger.begin(57600);
 
   if (finger.verifyPassword()) {
     Serial.println("Sensor de huellas conectado correctamente.");
   } else {
     Serial.println("No se puede conectar con el sensor de huellas.");
-    while (1) { delay(1); } // Espera indefinidamente si el sensor no se conecta
+    while (1) { delay(1); }  // Espera indefinidamente si el sensor no se conecta
   }
 }
 
@@ -29,18 +28,33 @@ void loop() {
   p = finger.getImage();
   
   if (p == FINGERPRINT_OK) {
-    fingerPresentCount++; // Incrementa el contador cuando se detecta un dedo
-    if (fingerPresentCount > 2) { // Si se detecta 3 veces consecutivas
+    fingerPresentCount++;  // Incrementa el contador cuando se detecta un dedo
+    if (fingerPresentCount > 3) {  // Si se detecta 3 veces consecutivas
       Serial.println("Dedo detectado.");
       fingerPresentCount = 0;  // Reinicia el contador
+
+      // Realizar una búsqueda rápida para confirmar si la huella está en la base de datos
+      p = finger.image2Tz();  // Convertir imagen a plantilla
+      if (p != FINGERPRINT_OK) {
+        Serial.println("Error en la conversión de imagen.");
+        return;
+      }
+      
+      p = finger.fingerFastSearch();  // Buscar la huella en la base de datos
+      if (p == FINGERPRINT_OK) {
+        Serial.println("Huella encontrada en la base de datos.");
+        Serial.print("ID: "); Serial.println(finger.fingerID);
+      } else {
+        Serial.println("Huella no encontrada.");
+      }
     }
   } else if (p == FINGERPRINT_NOFINGER) {
-    fingerPresentCount = 0; // Reinicia el contador si no hay dedo
+    fingerPresentCount = 0;  // Reinicia el contador si no hay dedo
     Serial.println("No hay dedo presente.");
   } else {
-    fingerPresentCount = 0; // Reinicia el contador en caso de error
+    fingerPresentCount = 0;  // Reinicia el contador en caso de error
     Serial.println("Error al leer el sensor.");
   }
-  
-  delay(500);  // Pausa corta entre lecturas
+
+  delay(1000);  
 }
