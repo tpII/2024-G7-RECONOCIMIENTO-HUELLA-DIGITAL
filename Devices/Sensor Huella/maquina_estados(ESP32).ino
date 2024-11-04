f34269df1c034c65d48c9b28e3e8e5dfed1de898
Azul Martini <azul.martini@alu.ing.unlp.edu.ar>
2024-11-04 01:19:51 -0300

    Update maquina_estados(ESP32).ino

--- Devices/Sensor Huella/maquina_estados(ESP32).ino
+++ Devices/Sensor Huella/maquina_estados(ESP32).ino
@@ -1,25 +1,90 @@
 #include <Adafruit_Fingerprint.h>
+#include "pitches.h"
+#include <Wire.h>
+#include "rgb_lcd.h"
 
 HardwareSerial mySerial(2);
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+// VARIABLES DEL PROGRAMA
 
+// LED /////////////////////////////////////
+#define RGB_GREEN 15
+#define RGB_BLUE 4
+#define RGB_RED 25
+
+#define espera 300
+#define duracion 500  // 5 segundos
+// BUZZER //////////////////////////////////
+#define BUZZZER_PIN  5
+
+// LCD /////////////////////////////////////
+rgb_lcd lcd;
+int colorR = 0;
+int colorG = 0;
+int colorB = 255;
+int tamDisplay = 16;
+
+byte check[8] = {
+B00000,
+B00001,
+B00001,
+B00010,
+B00010,
+B00100,
+B10100,
+B01000,
+};
+
+byte neg[8] = {
+B00000,
+B10001,
+B01010,
+B00100,
+B00100,
+B01010,
+B10001,
+B00000,
+};
+
+
+// HUELLA DACTILAR /////////////////////////
 uint8_t p=0;
 uint8_t fingerPresentCount = 0;  // Contador para verificar si realmente hay un dedo
 uint8_t estado = 1;  // Estado inicial (1: Lectura constante)
 uint8_t id;
 uint8_t nuevoEstado=0;
 uint8_t aux = 1;
+///////////////////////////////////////////
+
+void apagarRGB() {
+  pinMode(RGB_RED, 255);
+  pinMode(RGB_GREEN,255);
+  pinMode(RGB_BLUE, 255);
+  delay(200);
+}
 
 void setup() {
+  // LED SETUP
+  pinMode(RGB_RED, OUTPUT);
+  pinMode(RGB_GREEN, OUTPUT);
+  pinMode(RGB_BLUE, OUTPUT);
+  // LCD SETUP
+  lcd.begin(16, 2);  // Inicializa el LCD
+  lcd.setRGB(colorR, colorG, colorB);  // Establece el color del fondo
+  lcd.createChar(0,check);
+  lcd.createChar(1,neg);
+
+  // Fijar el texto en la segunda fila
+  lcd.setCursor(6, 1);  // Coloca el cursor en la segunda fila
+  continuoLCD();
+  
+  // HUELLA Y SERIAL SETUP
   Serial.begin(115200);
   while(!Serial);
   delay(1000);
   Serial.println("Inicializando sensor de huellas...");
-  // RX2 con TX sensor de huellas
-  // TX2 con RX sensor de huellas 
   mySerial.begin(57600, SERIAL_8N1, 16, 17);
-
   delay(100);
   if (finger.verifyPassword()) {
     Serial.println(F("Sensor de huellas conectado correctamente."));
@@ -59,7 +124,6 @@ void loop() {
       Serial.println(estado);
     }
   } 
-
   // Máquina de estados
   switch (estado) {
     case 1:
@@ -90,6 +154,7 @@ void loop() {
 }
 
 uint8_t LecturaConstante() {
+  continuoLCD();
   p = finger.getImage();
   if (p == FINGERPRINT_OK) {
     p = finger.image2Tz();  // Convertir imagen a plantilla
@@ -134,6 +199,7 @@ uint8_t LecturaConstante() {
 }
 
 void huellaEncontrada() {
+  casoEXITO();
   Serial.println("Huella encontrada en la base de datos.");
   Serial.print("ID: ");
   Serial.println(finger.fingerID);
@@ -142,6 +208,7 @@ void huellaEncontrada() {
 }
 
 void huellaNoEncontrada() {
+  casoFALLO();
   Serial.println("Huella no encontrada.");
 }
 
@@ -312,4 +379,97 @@ uint8_t CantidadHuellas(){
   } else {
     Serial.println("Error al contar las huellas almacenadas.");
   }
+}
+
+
+int melodiaCorrecto[] = {
+  NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5
+};
+
+// Caso LED
+int melodiaDenegado[] = {
+  NOTE_B1, NOTE_B1, NOTE_B1, NOTE_B1
+};
+
+// Duraciones de las notas 
+int noteDurations[] = {
+  4, 4, 4, 6
+};
+
+void continuoLCD() {
+  apagarRGB();
+  colorR = 0;
+  colorG = 0;
+  colorB = 255;
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setCursor(0, 0);  
+  lcd.print(" Ingrese huella ");  
+  lcd.setCursor(7, 1);  
+  lcd.write("HORA");  
+  delay(350); 
+}
+
+void casoEXITO(){
+  int greenIntensity = 0; 
+  int blueIntensity = 255;     
+  int redIntensity = 255;      
+  colorR = 0;
+  colorG = 150;
+  colorB = 0;
+  
+  // Establecer los colores
+  analogWrite(RGB_GREEN, greenIntensity);
+  analogWrite(RGB_BLUE, blueIntensity);
+  analogWrite(RGB_RED, redIntensity);
+
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setCursor(0, 0);  
+  lcd.print("ACCESO CORRECTO");  
+  lcd.setCursor(7, 1);  
+  lcd.print("               "); 
+  lcd.setCursor(7, 1); 
+  lcd.write(byte(0));
+
+  // Reproduce la melodía
+  for (int thisNote = 0; thisNote < 4; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZZER_PIN, melodiaCorrecto[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(BUZZZER_PIN);
+  }
+  apagarRGB();
+}
+
+void casoFALLO(){
+  int greenIntensity = 255; 
+  int blueIntensity = 255;     
+  int redIntensity = 0;      
+  colorR = 150;
+  colorG = 0;
+  colorB = 0;
+  
+  // Establecer los colores
+  analogWrite(RGB_GREEN, greenIntensity);
+  analogWrite(RGB_BLUE, blueIntensity);
+  analogWrite(RGB_RED, redIntensity);
+
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setCursor(0, 0);  
+  lcd.print("ACCESO DENEGADO");  
+  lcd.setCursor(7, 1);  
+  lcd.print("               "); 
+  lcd.setCursor(7, 1); 
+  lcd.write(byte(1));  
+  delay(300);
+
+  // Reproduce la melodía
+  for (int thisNote = 0; thisNote < 4; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZZER_PIN, melodiaDenegado[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+  }
+  apagarRGB();
 }
